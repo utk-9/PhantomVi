86ce3d452690a437df62faa7101f7f20e1a7f0e7
utk-9 <utkarshlamba.2@gmail.com>
2015-10-04 12:23:37 -0400

    Setting sceen/activity working

---
+++ app/src/main/java/cmon/phantomvib/SettingsActivity.java
@@ -0,0 +1,15 @@
+package cmon.phantomvib;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Welcome on 10/4/2015.
+ */
+public class SettingsActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_layout);
+    }
+}
